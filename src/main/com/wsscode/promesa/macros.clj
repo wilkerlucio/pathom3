94cbf7101f56ac01a4541c8bdd36dbe908ef0774
Wilker Lucio <wilkerlucio@gmail.com>
2021-01-20 13:26:36 -0300

    Add clet docstring

--- src/main/com/wsscode/promesa/macros.clj
+++ src/main/com/wsscode/promesa/macros.clj
@@ -2,7 +2,12 @@
   (:require
     [promesa.core :as p]))
 
-(defmacro clet [bindings & body]
+(defmacro clet
+  "This is similar to promesa let. But this only returns a promise if some of
+  the bindings is a promise. Otherwise returns values as-is. This function is
+  intended to use in places that you want to be compatible with both sync
+  and async processes."
+  [bindings & body]
   (assert (even? (count bindings)))
   (let [binds (reverse (partition 2 bindings))]
     (reduce
